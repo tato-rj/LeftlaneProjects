d4796c0e58e79d24919ed65b909a761084379564
Arthur Villar <=>
2018-12-01 18:13:33 -0500

    Improve icon on pieces results

--- resources/views/projects/pianolit/components/play.blade.php
+++ resources/views/projects/pianolit/components/play.blade.php
@@ -1,7 +1,11 @@
 @if($piece->hasAudio())
 <div class="d-inline position-relative">
 	<a href="{{$piece->file_path('audio_path')}}" target="_blank" class="text-brand mr-1"><i class="fas fa-microphone"></i></a>
-	<div class="position-absolute rounded-circle bg-{{$piece->recordingsAvailable() == 3 ? 'success' : 'secondary'}} text-white font-weight-bold d-flex align-items-center justify-content-center" style="bottom: 0; right: 2px; width: 10px; height: 10px; font-size: .5em">{{$piece->recordingsAvailable()}}</div>
+	@if($piece->recordingsAvailable() == 3)
+	<div class="position-absolute rounded-circle bg-success text-white font-weight-bold d-flex align-items-center justify-content-center" style="bottom: 0; right: 2px; width: 10px; height: 10px; font-size: .5em"><i class="fas fa-check"></i></div>	
+	@else
+	<div class="position-absolute rounded-circle bg-secondary text-white font-weight-bold d-flex align-items-center justify-content-center" style="bottom: 0; right: 2px; width: 10px; height: 10px; font-size: .5em">{{$piece->recordingsAvailable()}}</div>
+	@endif
 </div>
 @elseif($piece->youtube_array)
 <a href="https://www.youtube.com/watch?v={{$piece->youtube_array[0]}}" target="_blank" class="text-brand mr-1"><i class="fab fa-youtube"></i></a>
