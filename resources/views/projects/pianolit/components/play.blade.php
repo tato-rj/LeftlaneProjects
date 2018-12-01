ad81b7581adaefe86812acea0a73de7cd1c1d70d
Arthur Villar <=>
2018-12-01 18:16:21 -0500

    Improve icon on pieces results

--- resources/views/projects/pianolit/components/play.blade.php
+++ resources/views/projects/pianolit/components/play.blade.php
@@ -2,7 +2,7 @@
 <div class="d-inline position-relative">
 	<a href="{{$piece->file_path('audio_path')}}" target="_blank" class="text-brand mr-1"><i class="fas fa-microphone"></i></a>
 	@if($piece->recordingsAvailable() == 3)
-	<div class="position-absolute rounded-circle bg-success text-white font-weight-bold d-flex align-items-center justify-content-center" style="bottom: 0; right: 2px; width: 10px; height: 10px; font-size: .5em"><i class="fas fa-check"></i></div>	
+	<div class="position-absolute rounded-circle bg-success text-white font-weight-bold d-flex align-items-center justify-content-center" style="bottom: 0; right: 2px; width: 10px; height: 10px; font-size: .4em"><i class="fas fa-check"></i></div>	
 	@else
 	<div class="position-absolute rounded-circle bg-secondary text-white font-weight-bold d-flex align-items-center justify-content-center" style="bottom: 0; right: 2px; width: 10px; height: 10px; font-size: .5em">{{$piece->recordingsAvailable()}}</div>
 	@endif
