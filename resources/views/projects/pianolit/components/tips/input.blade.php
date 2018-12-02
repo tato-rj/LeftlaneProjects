e60d137b8be6932944dc3c4574e91bed11b70aba
Arthur Villar <=>
2018-12-01 21:15:17 -0500

    Add lines to tips

--- resources/views/projects/pianolit/components/tips/input.blade.php
+++ resources/views/projects/pianolit/components/tips/input.blade.php
@@ -1,9 +1,9 @@
 <div class="form-group type-container {{$type or 'd-flex'}} mb-2" style="display: {{$display or null}}">
 
-	<a class=" btn btn-sm btn-danger text-white mr-1 remove-field" style="height: 32px;">
+	<a class="align-self-stretch btn btn-sm btn-danger text-white mr-1 remove-field">
 		<i class="fas fa-minus"></i>
 	</a>
 
-  	<textarea rows="3" class="form-control-sm form-control" name="{{$name or null}}">{{$value or null}}</textarea>
+  	<input rows="1" class="form-control-sm form-control" name="{{$name or null}}" value="{{$value or null}}">
 
 </div>
