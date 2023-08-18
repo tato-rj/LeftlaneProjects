07b5a1b32b396d550f0269e236d3dc046516051f
Arthur Villar <arthur@iMac.local>
2023-08-18 14:10:08 -0400

    Quick update

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ Auth::routes();
 Route::post('/{guard}/logout', 'Auth\LoginController@logoutFrom')->name('logoutFrom');
 
 Route::get('/', function () {
-	return phpversion();
 	// \Artisan::call('cache:clear');
     return view('leftlane/index');
 });
