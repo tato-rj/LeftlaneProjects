0167161518d2630fa47d479f3802ac20aa764398
Arthur Villar <arthur@iMac.local>
2023-09-17 00:38:33 -0400

    Quick update

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -28,7 +28,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     protected function gate(): void
     {
         Gate::define('viewHorizon', function ($user = null) {
-            return auth()->check() ? auth()->user()->email == 'arthurvillar@gmail.com' : false;
+            return \Auth::guard('videouploader-admin')->check();
             // return in_array($user->email, [
             //     $user->email == 'arthurvillar@gmail.com'
             // ]);
