dabc46fa68b5fd9bb143ecea0e6c22cd1da9e666
Arthur Villar <arthur@iMac.local>
2023-10-18 22:53:39 -0400

    Quick update

--- resources/views/projects/videouploader/record/edit.blade.php
+++ resources/views/projects/videouploader/record/edit.blade.php
@@ -22,7 +22,6 @@
           @if($video->isCompleted())
             @include('projects.videouploader.record.actions.orientation')
           @elseif($video->isPending())
-            @include('projects.videouploader.record.actions.orientation')
             @include('projects.videouploader.record.actions.status')
           @elseif($video->isAbandoned() || $video->isFailed())
             @include('projects.videouploader.record.actions.retry')
