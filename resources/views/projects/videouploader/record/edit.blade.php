aa65f3b549bc67e8c1316e17c8689d88f34ff96a
Arthur Villar <arthur@iMac.local>
2023-10-19 10:39:15 -0400

    Quick update

--- resources/views/projects/videouploader/record/edit.blade.php
+++ resources/views/projects/videouploader/record/edit.blade.php
@@ -22,7 +22,7 @@
           @if($video->isCompleted())
             @include('projects.videouploader.record.actions.orientation')
           @elseif($video->isPending())
-            <div class="text-muted text-center small fst-italic">Waiting for {{$video->created_at->longAbsoluteDiffForHumans()}}</div>
+            <div class="text-warning text-center small fst-italic">@fa(['icon' => 'hourglass-half'])Waiting for {{$video->created_at->longAbsoluteDiffForHumans()}}</div>
           @elseif($video->isAbandoned() || $video->isFailed())
             @include('projects.videouploader.record.actions.retry')
           @endif
