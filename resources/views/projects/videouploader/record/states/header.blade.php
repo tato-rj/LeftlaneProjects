347f8f102fe97b4c3ca424c97955944a467af218
Arthur Villar <arthur@iMac.local>
2023-10-19 14:48:49 -0400

    Quick update

--- resources/views/projects/videouploader/record/states/header.blade.php
+++ resources/views/projects/videouploader/record/states/header.blade.php
@@ -1,4 +1,4 @@
-<div class="text-truncate">
+<div class="text-truncate pe-3">
 @if($video->isRemote())
 #{{$video->id}} | {{$video->created_at->toFormattedDateString()}} @fa(['icon' => $video->origin_icon, 'mr' => 2, 'ml' => 2]) {{$video->user_email}} - Piece ID: {{$video->piece_id}}
 @else
