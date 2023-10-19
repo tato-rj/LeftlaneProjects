638a32f20bf6b2cc70559905e291fff1be464a68
Arthur Villar <arthur@iMac.local>
2023-10-19 14:46:50 -0400

    Quick update

--- resources/views/projects/videouploader/record/create.blade.php
+++ resources/views/projects/videouploader/record/create.blade.php
@@ -15,18 +15,3 @@
     </div>
   </div>
 </div>
-
-{{-- <form method="POST" action="{{route('videouploader.upload')}}" enctype="multipart/form-data">
-    @csrf
-
-    <input type="hidden" name="secret" value="{{auth()->user()->tokens()->exists() ? auth()->user()->tokens->first()->name : null}}">
-    <input type="hidden" name="email" value="test@email.com">
-    <input type="hidden" name="origin" value="local">
-    <input type="hidden" name="user_id" value="1">
-    <input type="hidden" name="piece_id" value="1">
-
-    <div class="mb-3">
-      <input class="form-control" type="file" id="video" name="video" required>
-    </div>
-    <button type="submit" class="btn btn-primary">Upload</button>
-</form> --}}
--- resources/views/projects/videouploader/record/list.blade.php
+++ resources/views/projects/videouploader/record/list.blade.php
@@ -39,20 +39,20 @@
     @if($videos->isEmpty())
         <div class="d-flex justify-content-center pt-5" style="font-size: 8rem; opacity: .1;">@fa(['icon' => 'box-open'])</div>
     @else
-    <div class="accordion shadow-lg mb-3" id="records-container">
-    @foreach($videos as $video)
-        @if($video->isCompleted())
-        @include('projects.videouploader.record.states.completed')
-        @elseif($video->isFailed())
-        @include('projects.videouploader.record.states.failed')
-        @elseif($video->isAbandoned())
-        @include('projects.videouploader.record.states.abandoned')
-        @else
-        @include('projects.videouploader.record.states.pending')
-        @endif
-    @endforeach
-    </div>
+        <div class="accordion shadow-lg mb-3" id="records-container">
+        @foreach($videos as $video)
+            @if($video->isCompleted())
+            @include('projects.videouploader.record.states.completed')
+            @elseif($video->isFailed())
+            @include('projects.videouploader.record.states.failed')
+            @elseif($video->isAbandoned())
+            @include('projects.videouploader.record.states.abandoned')
+            @else
+            @include('projects.videouploader.record.states.pending')
+            @endif
+        @endforeach
+        </div>
 
-    {{$videos->links()}}
+        {{$videos->links()}}
     @endif
 </div>
