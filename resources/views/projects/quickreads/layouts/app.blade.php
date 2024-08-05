183a96df3983711fdc37779e8384993a2042468d
Arthur Villar <arthur@iMac.local>
2024-08-05 09:54:18 -0400

    Quick update

--- resources/views/projects/quickreads/layouts/app.blade.php
+++ resources/views/projects/quickreads/layouts/app.blade.php
@@ -28,6 +28,7 @@
   @endif
 
   <script type="text/javascript" src="{{mix('js/app.js')}}"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
   <script type="text/javascript" src="{{asset('js/vendor/jquery.easing.min.js')}}"></script>
   <script type="text/javascript" src="{{asset('js/vendor/Chart.min.js')}}"></script>
