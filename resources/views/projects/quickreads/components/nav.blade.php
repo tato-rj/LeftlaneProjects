e47794bd57edb631238d3243f873cb25ebdf174e
Arthur Villar <arthur@iMac.local>
2024-08-04 22:14:10 -0400

    Quick update

--- resources/views/projects/quickreads/components/nav.blade.php
+++ resources/views/projects/quickreads/components/nav.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+{{-- <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
   <a class="navbar-brand" href="/quickreads/"><img src="{{asset('images/icon.svg')}}" class="mr-2">Quickreads</a>
   <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -89,4 +89,4 @@
         </li>
       </ul>
     </div>
-  </nav>
+  </nav> --}}
