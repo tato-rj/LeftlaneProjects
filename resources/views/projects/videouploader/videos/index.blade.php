4c41a04621fe7a836373f527af1b820824c45271
Arthur Villar <arthur@iMac.local>
2024-09-25 15:16:39 -0400

    Quick update

--- resources/views/projects/videouploader/videos/index.blade.php
+++ resources/views/projects/videouploader/videos/index.blade.php
@@ -17,4 +17,33 @@
 @endsection
 
 @push('scripts')
+<script type="text/javascript">
+let debounceTimer;
+
+$('input#table-search').on('keyup', function() {
+    clearTimeout(debounceTimer);
+
+    let input = $(this).val();
+    debounceTimer = setTimeout(function() {
+        let url = updateUrl('input', input);
+
+        // axios.get(url)
+        //     .then(function(response) {
+        //         $('#results-container').html(response.data);
+
+        //         if (input.length > 1)
+        //             $('.highlight-match').mark(input);
+        //     })
+        //     .catch(function(error) {
+        //         log(error);
+        //     });
+    }, 300);
+});
+
+function updateUrl(key, value) {
+    var url = new URL(window.location.href);
+    url.searchParams.set(key, value);
+    return url;
+}
+</script>
 @endpush
