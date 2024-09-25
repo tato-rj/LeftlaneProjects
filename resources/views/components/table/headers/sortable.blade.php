37d22b5a86272890abd31381640a2adb9fe62fc5
Arthur Villar <arthur@iMac.local>
2024-09-25 11:16:54 -0400

    Quick update

--- resources/views/components/table/headers/sortable.blade.php
+++ resources/views/components/table/headers/sortable.blade.php
@@ -3,7 +3,7 @@
 @form(['method' => 'GET', 'url' => url()->full(), 'csrf' => false])
 <input type="hidden" name="sort_by" value="{{(new \Table)->getFieldname($field)}}">
 <input type="hidden" name="order_by" value="{{$selected && request()->order_by == 'asc' ? 'desc' : 'asc'}}">
-<button type="submit" class="d-apart btn-raw w-100
+<button type="submit" class="d-flex justify-content-between align-items-center btn-raw w-100
   @if(request()->has('sort_by'))
   @unless($selected && request()->sort_by == (new \Table)->getFieldname($field))
   opacity-4
