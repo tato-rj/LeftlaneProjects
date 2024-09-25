56b452bd028664e28ce4d44d64c8d0f85d7edb68
Arthur Villar <arthur@iMac.local>
2024-09-25 11:27:20 -0400

    Quick update

--- resources/views/components/table/searchbar.blade.php
+++ resources/views/components/table/searchbar.blade.php
@@ -1,12 +1,6 @@
 <div class="container mb-4">
 	<div class="row">
-		<div class="
-		@if(subdomain('admin'))
-		{{$layout ?? 'col-lg-3 col-md-4 col-8 ml-auto'}}
-		@else
-		{{$layout ?? 'col-lg-4 col-md-6 col-10 mx-auto'}}
-		@endif
-		">
+		<div class="{{$layout ?? 'col-lg-4 col-md-6 col-10 mx-auto'}}">
 			<div class="border-bottom w-100 d-flex align-items-center">
 				@fa(['icon' => 'search', 'mr' => 0])
 				<input id="{{$id ?? 'table-search'}}" type="text" autocomplete="off" name="{{$name ?? 'search'}}" class="form-control border-0 m-0" placeholder="Search here...">
