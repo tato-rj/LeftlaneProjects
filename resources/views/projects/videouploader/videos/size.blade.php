98b6c30b2c522c032b57a579859fb19ff1260379
Arthur Villar <arthur@iMac.local>
2024-09-26 11:56:06 -0400

    Quick update

--- resources/views/projects/videouploader/videos/size.blade.php
+++ resources/views/projects/videouploader/videos/size.blade.php
@@ -1,6 +1,6 @@
 <div class="container mb-4">
 	<div class="w-100 bg-light border" style="height: 20px">
-		<div class="h-100 d-flex align-items-center justify-content-end pe-2" style="
+		<div class="h-100 d-flex align-items-center justify-content-end pe-2 small" style="
 			width: 10%;
 			background: lightgreen;
 		">10%</div>
