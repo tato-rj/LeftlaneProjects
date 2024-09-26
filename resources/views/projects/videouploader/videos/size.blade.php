c152b0c078b933f8a2d7c49d14a47ad67c823bc4
Arthur Villar <arthur@iMac.local>
2024-09-26 12:03:31 -0400

    Quick update

--- resources/views/projects/videouploader/videos/size.blade.php
+++ resources/views/projects/videouploader/videos/size.blade.php
@@ -1,17 +1,13 @@
 @php($size = bytesToGb(\App\Projects\VideoUploader\Video::sum('original_size')))
 @php($percentage = percentage($size, 20))
 
-@switch($percentage)
-    @case(< 50)
-        @php($color = 'lightgreen')
-        @break
-    @case(< 90)
-        @php($color = 'lightyellow')
-        @break
-    @default
-        @php($color = 'lightred')
-@endswitch
-
+@if($percentage < 50)
+@php($color = 'lightgreen')
+@elseif($percentage < 90)
+@php($color = 'lightyellow')
+@else
+@php($color = 'lightyellow')
+@endif
 
 <div class="container mb-4">
 	<div class="w-100 bg-light border" style="height: 20px">
