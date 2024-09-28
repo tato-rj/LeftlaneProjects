2df932bef78eaf9a93f9d8ba5e5f1c2c4fb88371
Arthur Villar <arthur@iMac.local>
2024-09-28 17:40:42 -0400

    Quick update

--- app/Http/Middleware/AcceptApiRequests.php
+++ app/Http/Middleware/AcceptApiRequests.php
@@ -16,9 +16,9 @@ class AcceptApiRequests
      */
     public function handle(Request $request, Closure $next)
     {
-        if ($request->has('secret'))
+        if ($request->has('token'))
             abort(404, 'HAS SECRET');
-        
+
         if (! $request->has('secret') || ! \DB::connection('videouploader')->table('personal_access_tokens')->where('name', $request->secret)->exists())
             throw new \Illuminate\Auth\Access\AuthorizationException('Token mismatch, you are not authorized to do this.');
 
