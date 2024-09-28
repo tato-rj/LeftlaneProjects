c8524950ec9016418d479713782b40adae77e411
Arthur Villar <arthur@iMac.local>
2024-09-28 17:34:38 -0400

    Quick update

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
+        'videouploader/fix',
         'videouploader/upload',
         'videouploader/delete',
         'quickreads/password/email',
